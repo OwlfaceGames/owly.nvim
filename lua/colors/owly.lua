0c8c251b7ac71439c489fed936d341379888a775
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2026-02-24 02:17:05 +0000

    update background color

--- lua/colors/owly.lua
+++ lua/colors/owly.lua
@@ -8,7 +8,7 @@ local colors = {
   cyan       = "#53d549",
   violet     = "#AE81FF",
 
-  background = "#2e2927",
+  background = "#212121",
   gutter     = "#4B5345",
   selection  = "#563e57",
   text       = "#AF9374",
