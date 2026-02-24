266e18954574469ddf84393da70e673eff1d1180
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2026-02-24 15:10:38 +0000

    tweak lualine bg

--- lua/colors/owly.lua
+++ lua/colors/owly.lua
@@ -27,7 +27,7 @@ local colors = {
   highlight  = "#334230",
   line_fg    = "#4B5345",
   lualine_fg = "#AF9374",
-  lualine_bg = "#0e0e0e",
+  lualine_bg = "#151515",
 
   dimmed_keyword = "#b0b0b0",
   dimmed_function = "#cccccc",
